bee574715011dec795d0a3c6c9cdae8d974cbe6b
Izzy4 <baielaibek@mail.ru>
2022-06-15 18:30:40 +0600

    detail(correct)

---
+++ app/src/main/java/com/example/animeapp/ui/fragment/detail/DetailFragment.kt
@@ -0,0 +1,12 @@
+package com.example.animeapp.ui.fragment.detail
+
+import com.example.animeapp.databinding.FragmentDetailBinding
+import com.example.core.base.BaseFragment
+
+class DetailFragment :
+    BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
+
+    override fun setupUI() {
+
+    }
+}
---
+++ app/src/main/java/com/example/animeapp/ui/fragment/detail/DetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.animeapp.ui.fragment.detail
+
+import com.example.core.base.BaseViewModel
+import javax.inject.Inject
+
+class DetailViewModel @Inject constructor() :
+    BaseViewModel() {
+
+
+
+}
